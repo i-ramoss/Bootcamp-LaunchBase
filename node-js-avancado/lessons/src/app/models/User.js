815c1d890d76c3cfe78f03550eea55b988ae0d93
Ian_Ramos <ianramossantos@hotmail.com>
2021-01-21 09:54:01 -0300

    refactor: Models refactoring according to the MVC Architecture
    - Create a Base model for the common requests among the other Models

--- node-js-avancado/lessons/src/app/models/User.js
+++ node-js-avancado/lessons/src/app/models/User.js
@@ -1,10 +1,12 @@
-const db = require("../../config/db")
-const { hash } = require("bcryptjs")
-const fs = require("fs")
+const Base = require("./Base")
 
-const Product = require("./Product")
+Base.init({ table: "users" })
 
 module.exports = {
+  ...Base,
+}
+
+/*
   async create(data) {
     try {
       const query = `
@@ -38,30 +40,6 @@ module.exports = {
     }
   },
 
-  async findOne(filters) {
-    try {
-      let query = `SELECT * FROM users`
-
-      Object.keys(filters).map( key => {
-        query = `
-          ${query}
-          ${key}
-        `
-  
-        Object.keys(filters[key]).map( field => {
-          query = `${query} ${field} = '${filters[key][field]}'`
-        })
-      })
-  
-      const results = await db.query(query)
-      
-      return results.rows[0]
-    }
-    catch (err) {
-      console.error(err)
-    }
-  },
-
   async update(id, fields) {
     let query = "UPDATE users SET"
 
@@ -84,7 +62,7 @@ module.exports = {
     await db.query(query)
     return
   },
-
+  
   async delete(id) {
     try {
       let results = await db.query("SELECT * FROM products WHERE user_id = $1", [id])
@@ -111,4 +89,4 @@ module.exports = {
       console.error(err)
     }
   }
-}
+*/
