f034a0a06e964244591b70c588927c110ed3faa0
Ian_Ramos <ianramossantos@hotmail.com>
2021-03-01 10:25:34 -0300

    Add orders table

--- node-js-avancado/lessons/database.sql
+++ node-js-avancado/lessons/database.sql
@@ -48,10 +48,26 @@ CREATE TABLE "users" (
   "updated_at" timestamp DEFAULT (now())
 );
 
+CREATE TABLE "orders" (
+	"id" SERIAL PRIMARY KEY,
+  "seller_id" int NOT NULL,
+  "buyer_id" int NOT NULL,
+  "product_id" int NOT NULL,
+  "price" int NOT NULL,
+  "quantity" int DEFAULT 0,
+  "total" int NOT NULL,
+  "status" text NOT NULL,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
+);
+
 -- Foreign keys:
 ALTER TABLE "products" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
 ALTER TABLE "files" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
 ALTER TABLE "products" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "orders" ADD FOREIGN KEY ("seller_id") REFERENCES "users" ("id");
+ALTER TABLE "orders" ADD FOREIGN KEY ("buyer_id") REFERENCES "users" ("id");
+ALTER TABLE "orders" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
 
 -- Create procedure
 CREATE FUNCTION trigger_set_timestamp()
@@ -73,6 +89,12 @@ EXECUTE PROCEDURE trigger_set_timestamp();
 CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON users
 FOR EACH ROW 
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+-- auto updated_at orders
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON orders
+FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
 
 -- Create session table
