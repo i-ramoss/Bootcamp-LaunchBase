3febcb241ee17f76d2b7827eedebef3596434944
Ian_Ramos <ianramossantos@hotmail.com>
2021-03-01 09:21:32 -0300

    feat: Add, remove and delete products to the cart

--- node-js-avancado/lessons/src/app/controllers/CartController.js
+++ node-js-avancado/lessons/src/app/controllers/CartController.js
@@ -1,3 +1,4 @@
+const { addOne } = require("../../lib/cart")
 const Cart = require("../../lib/cart")
 
 const LoadProductsService = require("../services/LoadProductService")
@@ -7,14 +8,45 @@ module.exports = {
     try {
       let { cart } = request.session
 
-      const product = await LoadProductsService.load("product", { where: { id: 2} })
-
-      cart = Cart.init(cart).addOne(product)
+      cart = Cart.init(cart)
 
       return response.render("cart/index", { cart })
     }
     catch (err) {
       console.error(err)
     }
+  },
+
+  async addOne(request, response) {
+    const { id } = request.params
+    let { cart } = request.session
+
+    const product = await LoadProductsService.load("product", { where: { id } })
+    
+    request.session.cart = Cart.init(cart).addOne(product)
+
+    return response.redirect("/cart")
+  },
+
+  removeOne(request, response) {
+    const { id } = request.params
+    let { cart } = request.session
+
+    if (!cart) return response.redirect("/cart")
+
+    request.session.cart = Cart.init(cart).removeOne(id)
+
+    return response.redirect("/cart")
+  },
+
+  deleteProduct(request, response) {
+    const { id } = request.params
+    let { cart } = request.session
+
+    if (!cart) return
+
+    request.session.cart = Cart.init(cart).delete(id)
+
+    return response.redirect("/cart")
   }
 }
--- node-js-avancado/lessons/src/routes/cart.js
+++ node-js-avancado/lessons/src/routes/cart.js
@@ -4,6 +4,9 @@ const CartController = require("../app/controllers/CartController")
 
 const routes = express.Router()
 
-routes.get("/", CartController.index)
+.get("/", CartController.index)
+.post("/:id/add-one", CartController.addOne)
+.post("/:id/remove-one", CartController.removeOne)
+.post("/:id/delete", CartController.deleteProduct)
 
 module.exports = routes
